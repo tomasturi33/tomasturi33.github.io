6d2a4250b06af66b39b5b9d857d061b527d02ce9
Tomas Turi <tomasturi@gmail.com>
2021-10-31 18:16:01 -0300

    first commit

---
+++ precache-manifest.021a703a46add07546a66a4b90360a74.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "07d3c24e8e4398dd9a19",
+    "url": "/css/app.c34d9160.css"
+  },
+  {
+    "revision": "07d3c24e8e4398dd9a19",
+    "url": "/js/app.56a8bbe3.js"
+  },
+  {
+    "revision": "bdda8698921ac61af643",
+    "url": "/js/chunk-2d219ff9.576870b4.js"
+  },
+  {
+    "revision": "b8b69ad63372e3e97ec1",
+    "url": "/css/chunk-vendors.db4413b5.css"
+  },
+  {
+    "revision": "b8b69ad63372e3e97ec1",
+    "url": "/js/chunk-vendors.80466dd0.js"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "/fonts/nucleo-icons.ff208b8d.woff2"
+  },
+  {
+    "revision": "8224e0160e362e117cbe00495919e2af",
+    "url": "/fonts/nucleo-icons.8224e016.eot"
+  },
+  {
+    "revision": "b0dc05d015e91e7d28d79cd0056fe555",
+    "url": "/fonts/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "dc0cdfac3187d5fe800b249201cdf9b0",
+    "url": "/img/nucleo-icons.dc0cdfac.svg"
+  },
+  {
+    "revision": "2124e56d15269ae57a715b67d65cf47e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/.gitkeep"
+  },
+  {
+    "revision": "99e58416b89637502b40ac8350eed85a",
+    "url": "/img/faces/face-1.jpg"
+  },
+  {
+    "revision": "512f0969f708e586816bdb9017c7d32c",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "40e93b11cbd4174c60b22cba0d630300",
+    "url": "/css/normalize.css"
+  },
+  {
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/img/default-avatar.png"
+  },
+  {
+    "revision": "be74132f42ae1d3502f9a9b9fff68ac1",
+    "url": "/Dashboard.PNG"
+  },
+  {
+    "revision": "0e953b1ea6d6e3addd7210e9c7c420a7",
+    "url": "/img/faces/face-2.jpg"
+  },
+  {
+    "revision": "4ab22eca4053c14a34e4bdb6390deae9",
+    "url": "/img/faces/face-5.jpg"
+  },
+  {
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/img/faces/face-0.jpg"
+  },
+  {
+    "revision": "4b87e628f4ef9988718860890b2a682f",
+    "url": "/img/faces/face-6.jpg"
+  },
+  {
+    "revision": "bf1684a30a86ba1b222aab3acff16356",
+    "url": "/img/faces/face-7.jpg"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/img/faces/face-3.jpg"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/img/favicon.png"
+  },
+  {
+    "revision": "43b98081492ac3bcb4a1fac6cf709403",
+    "url": "/img/faces/tim_vector.jpe"
+  },
+  {
+    "revision": "0b68eb8f1cde1fc9987a9196df05b96a",
+    "url": "/img/faces/face-4.jpg"
+  },
+  {
+    "revision": "d27fbc90c2e644dfdc9765640dc713b9",
+    "url": "/img/mask.png"
+  },
+  {
+    "revision": "f575a04ebbb31b5798a4c54783e745a2",
+    "url": "/img/new_logo.png"
+  },
+  {
+    "revision": "0ec8f86a54dce32152eaa33e932b9c97",
+    "url": "/img/loading-bubbles.svg"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/img/sidebar-2.jpg"
+  },
+  {
+    "revision": "897b4cf909210560a84398d36da51983",
+    "url": "/img/sidebar-4.jpg"
+  },
+  {
+    "revision": "7c375537bed5106ba29daa33092b1bfc",
+    "url": "/img/sidebar-6.jpg"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/img/sidebar-1.jpg"
+  },
+  {
+    "revision": "9ebea76ee1225f00d882b21547a7b49f",
+    "url": "/img/sidebar-5.jpg"
+  },
+  {
+    "revision": "44bf13a71a4db6e15913fe8af9296711",
+    "url": "/img/tim_80x80.png"
+  },
+  {
+    "revision": "cd253e23ed052deeb80b42d2ed772183",
+    "url": "/img/sidebar-3.jpg"
+  },
+  {
+    "revision": "c2a605fbc0e687b2e1b4b90a7c445cdd",
+    "url": "/img/vue-logo.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1d75384c628b21b83d100813de2c15aa",
+    "url": "/UNL_white_redondo.png"
+  },
+  {
+    "revision": "512f0969f708e586816bdb9017c7d32c",
+    "url": "/img/unl.png"
+  },
+  {
+    "revision": "512f0969f708e586816bdb9017c7d32c",
+    "url": "/rue/unl.png"
+  },
+  {
+    "revision": "ad89201ec9a8121ebdc5fdfbe6e87975",
+    "url": "/rue/rocket-launch-day-illustration.png"
+  },
+  {
+    "revision": "1d75384c628b21b83d100813de2c15aa",
+    "url": "/rue/UNL_white_redondo.png"
+  },
+  {
+    "revision": "a585f09aaf97b6f35e814b2fc24720a7",
+    "url": "/rue/rocket-launch-night-illustration.png"
+  }
+];
